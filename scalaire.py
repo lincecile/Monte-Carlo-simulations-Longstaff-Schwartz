42cc9721e47eea3fe4a4c5e6ea9648e5e2717d46
lincecile <lincecile7@gmail.com>
2025-02-20 21:12:40 +0100

    sclaire ok

--- scalaire.py
+++ scalaire.py
@@ -3,21 +3,19 @@ import numpy as np
 import scipy.stats as stats
 import random
 import pandas as pd
-random.seed(42)
 
-n = 1000 #precision de l'approximation
-N = 1000 # nombre de trajectoire
-T = 1
-dt = T / N  # Pas de temps
+import matplotlib.pyplot as plt
 
+
+np.random.seed(42)
 
 ###################################################################################
 ###################################  Scalaire  ####################################
 ###################################################################################
 
-T = 3 # maturité
-n = 5 # nb de pas
-N = 10 # nombre de trajectoire
+T = 1 # maturité
+n = 100 # nb de pas
+N = 2000 # nombre de trajectoire
 
 step = T/n # discretisation
 
@@ -27,10 +25,16 @@ W = np.zeros((n+1,N)) # matrice des valeurs du mouvement Brownien
 S0 = 100 # valeur initiale de U
 S = S0*np.ones((n+1,N)) # matrice n+1 x N rempli de la valeur initiale de  U
 
+# valeur option en T
+S_T = np.ones((1,N))
+
+# NPV chaque branche
+NPV = np.ones((1,N))
+
 K = 10
 sigma = 0.2
-r = 0.2
-q = 0.1
+r = 0.04
+q = 0
 
 for j in range(N): # N trajectoires representés par les colonnes de la matrice
     for i in range(1,n+1): # n pas representés par les lignes de la matrice
@@ -43,13 +47,31 @@ for j in range(N): # N trajectoires representés par les colonnes de la matrice
         
         # S_0 = 100
         # S_t+dt = S_t * exp((r-q) * dt + sigma*delta_brownian - sig^2 / 2 * dt)
-        S[i,j] = S[i-1,j]*np.exp((r - q) * (i - (i-1)) + sigma *(W[i,j]-W[i-1,j]) - sigma**2 / 2 * (i - (i-1)))
-
-S_T = S0*np.exp( (r - q - sigma**2 / 2)*T + sigma*W[-1,-1])
-
-NPV = np.mean(max(0,S_T - K))*np.exp(-r*T)
+        # S[i,j] = S[i-1,j]*np.exp((r - q) * (i - (i-1)) + sigma *(W[i,j]-W[i-1,j]) - sigma**2 / 2 * (i - (i-1)))
+    
+    S_T[0,j] = S0*np.exp( (r - q - sigma**2 / 2)*T + sigma*W[-1,j])
+    NPV[0,j] = max(0,S_T[0,j] - K)
+    
        
-print(pd.DataFrame(W))
-print()
-print(pd.DataFrame(S))
+NPV = np.mean(NPV)*np.exp(-r*T)
 print(NPV)
+
+exit()
+
+dates=np.linspace(0,T,n+1) # axes des abscisses
+
+fig, axs = plt.subplots(1, 2, figsize=(14, 5))  # 1 ligne, 2 colonnes
+
+# Premier graphique
+axs[0].plot(dates, W)
+axs[0].set_title('Mouvement Brownien')
+axs[0].set_xlabel('Temps t')
+
+# Deuxième graphique
+axs[1].plot(dates, S, linestyle='--')  # Courbes en pointillés
+axs[1].set_title('Trajectoire sous jacent')
+axs[1].set_xlabel('Temps t')
+
+# Ajustement pour éviter le chevauchement
+plt.tight_layout()
+plt.show()
