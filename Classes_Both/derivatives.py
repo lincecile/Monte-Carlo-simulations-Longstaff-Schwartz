bf57fa5f502cb23d10fed892701758ee40ad8b9d
emmaalaoui <emmaalaoui24@gmail.com>
2025-04-09 15:13:09 +0200

    ajout commentaires et vomma

--- Classes_Both/derivatives.py
+++ Classes_Both/derivatives.py
@@ -1,28 +1,29 @@
-import datetime as dt
 from dataclasses import dataclass
 from typing import Callable
 from Classes_Both.module_option import Option
 from Classes_Both.module_marche import DonneeMarche
 from Classes_MonteCarlo_LSM.module_brownian import Brownian
 from Classes_MonteCarlo_LSM.module_LSM import LSM_method
-from Classes_MonteCarlo_LSM.module_LSM import LSM_method
-from copy import deepcopy, copy
+from copy import deepcopy
+from typing import Union
+import datetime as dt
 
 """
-A data class to represent the parameters of an option derivative.
-Attributes:
-    price (float): The current price of the underlying asset.
-    strike (float): The strike price of the option.
-    maturity (float): The time to maturity of the option in years.
-    r (float): The risk-free interest rate.
-    sigma (float): The volatility of the underlying asset.
+Une classe de données représentant les paramètres d'une option dérivée.
+Attributs :
+    price (float) : Prix actuel de l'actif sous-jacent.
+    strike (float) : Prix d'exercice de l'option.
+    maturity (float) : Temps restant avant l'échéance (en années).
+    r (float) : Taux d'intérêt sans risque.
+    sigma (float) : Volatilité de l'actif sous-jacent.
 """
 @dataclass
 class OptionDerivativesParameters:
     option: Option  # Objet option
     market: DonneeMarche  # Objet marché
     
-    def __getitem__(self, item):
+    # getter
+    def __getitem__(self,  item: str)-> Union[float, dt.date]:
         if item == "price":
             return self.market.prix_spot
         elif item == "strike":
@@ -38,7 +39,8 @@ class OptionDerivativesParameters:
         else:
             raise KeyError(f"Paramètre inconnu: {item}")
 
-    def __setitem__(self, item, value):
+    # setter
+    def __setitem__(self, item: str, value: Union[float, dt.date]) -> None:
         if item == "price":
             self.market.prix_spot = value
         elif item == "strike":
@@ -58,52 +60,55 @@ class OptionDerivativesParameters:
 
 class OneDimDerivative: 
     """
-    A class to compute the first derivative of a given function with respect to one of its parameters.
-    -----------
-    Attributes:
-    f : Callable
-        The function for which the derivative is to be computed.
-    parameters : OptionDerivativesParameters
-        The parameters with which we want to compute the option derivative.
-    shift : float, optional
-        The shift value used for finite difference approximation (default is 1).
-    -----------
-    Methods:
-    first(along: str) -> float:
-        Computes the first derivative of the function with respect to the specified parameter.
+    Classe permettant de calculer la dérivée première d'une fonction par rapport à un paramètre donné.
+    Attributs :
+        f : Callable
+            La fonction pour laquelle la dérivée est calculée.
+        parameters : OptionDerivativesParameters
+            Paramètres utilisés pour l'évaluation de la fonction.
+        shift : float
+            Pas utilisé dans l'approximation par différences finies.
+    Méthodes :
+        first(along: str) -> float
+            Calcule la dérivée première par rapport au paramètre spécifié.
     """
     
-    def __init__(self, function:Callable, parameters: OptionDerivativesParameters, shift:float=1) -> None:
+    def __init__(self, function:Callable, parameters: OptionDerivativesParameters, shift:float=1, brownian=None) -> None:
         self.f = function
         self.parameters = parameters
         self.shift = shift
+        self.brownian = brownian
         
+    # Calcul de la dérivée première avec la formule des différences finies
     def first(self, along: str) -> float:
         params_u = deepcopy(self.parameters)
         params_u[along] += self.shift
         
         params_d = deepcopy(self.parameters)
         params_d[along] -= self.shift
-        return (self.f(params_u)[0] - self.f(params_d)[0]) / (2 * self.shift)
+        return (self.f(params_u, brownian=self.brownian)[0] - self.f(params_d, brownian=self.brownian)[0]) / (2 * self.shift)
 
 class TwoDimDerivatives:
     """
-    A class to compute second-order partial derivatives of a given function with respect to two parameters.
-    -----------
-    Attributes:
-        f (Callable): The function for which the derivatives are to be computed.
-        parameters (OptionDerivativesParameters): The parameters of the function.
-        shift (float): The shift value used for finite difference approximation. Default is 1.
-    -----------
-    Methods:
-        second(along1: str, along2: str) -> float:
-            Computes the second-order mixed partial derivative of the function with respect to the given parameters.
-    """
-    def __init__(self, function: Callable, parameters: OptionDerivativesParameters, shift: float = 1) -> None:
+    Classe permettant de calculer une dérivée seconde croisée d'une fonction par rapport à deux paramètres.
+    Attributs :
+        f : Callable
+            Fonction pour laquelle on veut la dérivée seconde.
+        parameters : OptionDerivativesParameters
+            Paramètres de la fonction.
+        shift : float
+            Pas de l'approximation en différences finies.
+    Méthodes :
+        second(along1: str, along2: str) -> float
+            Calcule la dérivée seconde croisée par rapport aux deux paramètres.
+"""
+    def __init__(self, function: Callable, parameters: OptionDerivativesParameters, shift: float = 1,brownian=None) -> None:
         self.f = function
         self.parameters = parameters
         self.shift = shift
+        self.brownian = brownian
 
+    # Calcul de la dérivée seconde avec la formule des différences finies 
     def second(self, along1: str, along2: str) -> float:
         params_uu = deepcopy(self.parameters)
         params_uu[along1] += self.shift
@@ -120,25 +125,38 @@ class TwoDimDerivatives:
         params_dd = deepcopy(self.parameters)
         params_dd[along1] -= self.shift
         params_dd[along2] -= self.shift
-        return (self.f(params_uu)[0] - self.f(params_ud)[0] - self.f(params_du)[0] + self.f(params_dd)[0]) / (4 * self.shift * self.shift)
+
+        numerator = (
+        self.f(params_uu, brownian=self.brownian)[0]
+        - self.f(params_ud, brownian=self.brownian)[0]
+        - self.f(params_du, brownian=self.brownian)[0]
+        + self.f(params_dd, brownian=self.brownian)[0]
+    )
+        denominator = 4 * self.shift * self.shift
+        return numerator / denominator
 
 
 class OptionDerivatives: 
     """
-    A class to compute the price and the Greeks (delta, vega, theta, gamma) of an option using numerical methods.
-    Attributes:
-        option (BaseOption): The option for which the derivatives are computed.
-        parameters (OptionDerivativesParameters): The parameters of the option.
-        pricer_options (dict): Other options for the pricer
+    Classe permettant de calculer le prix et les "Greeks" (delta, vega, theta, gamma, vomma) d'une option par méthode numérique.
+    Attributs :
+        option : Option
+            L'option concernée.
+        parameters : OptionDerivativesParameters
+            Paramètres de l'option (marché + caractéristiques).
+        pricer_options : dict
+            Options supplémentaires pour le pricer LSM.
     """
-    def __init__(self, option: Option, market: DonneeMarche, pricer : LSM_method):
+    def __init__(self, option: Option, market: DonneeMarche, pricer : LSM_method, pricer_options: dict = None)-> None:
         self.option = deepcopy(option)
         self.market = deepcopy(market)
         self.pricer = pricer
         self.parameters = OptionDerivativesParameters(option, market)
+        self.pricer_options = pricer_options or {}
         
-    def price(self, params: OptionDerivativesParameters):
-    # Mise à jour des valeurs
+    # Méthode pour calculer le prix avec les paramètres de l'option
+    def price(self, params: OptionDerivativesParameters, brownian : Brownian)-> float:
+    # Récupération des paramètres
         self.market.prix_spot = params["price"]
         self.market.volatilite = params["sigma"]
         self.market.taux_interet = params["r"]
@@ -147,23 +165,28 @@ class OptionDerivatives:
         self.option.maturity = params["maturity"]
 
         period = (self.option.maturite - self.option.date_pricing).days / 365
-        # print("Period : ", period)
-        # print(self.option.maturity)
-        brownian = Brownian(period, 200, 100000, 1)
+        brownian = Brownian(period, brownian.nb_step, brownian.nb_trajectoire, brownian.seed)
         pricer = LSM_method(self.option)
 
         # Pricing avec LSM
-        return pricer.LSM(brownian, self.market, method='vector')
+        return pricer.LSM(brownian, self.market, **self.pricer_options)
 
-
-    def delta(self): 
-        return OneDimDerivative(self.price, self.parameters, shift=1).first("price")
+    # Calcul du delta : sensibilité au prix du sous-jacent
+    def delta(self, brownian: Brownian)-> float:
+        return OneDimDerivative(self.price, self.parameters, shift=1, brownian=brownian).first("price")
     
-    def vega(self):
-        return OneDimDerivative(self.price, self.parameters, shift=0.01).first("sigma")
+    # Calcul du vega : sensibilité à la volatilité
+    def vega(self, brownian: Brownian)-> float:
+        return OneDimDerivative(self.price, self.parameters, shift=0.01, brownian=brownian).first("sigma")
     
-    def theta(self):
-        return -OneDimDerivative(self.price, self.parameters, shift=1/365).first("maturity")
+    # Calcul du theta : sensibilité au temps (à l'échéance)
+    def theta(self, brownian: Brownian)-> float:
+        return -OneDimDerivative(self.price, self.parameters, shift=1/365, brownian=brownian).first("maturity")
     
-    def gamma(self):
-        return TwoDimDerivatives(self.price, self.parameters).second("price", "price")
+    # Calcul du gamma : dérivée seconde par rapport au prix
+    def gamma(self, brownian: Brownian)-> float:
+        return TwoDimDerivatives(self.price, self.parameters, brownian=brownian).second("price", "price")
+    
+    # Calcul du vomma : dérivée seconde par rapport à la volatilité
+    def vomma(self, brownian: Brownian)-> float:
+        return TwoDimDerivatives(self.price, self.parameters, shift=0.01, brownian=brownian).second("sigma", "sigma")
--- Classes_MonteCarlo_LSM/module_brownian.py
+++ Classes_MonteCarlo_LSM/module_brownian.py
@@ -1,32 +1,28 @@
 import numpy as np
 import scipy.stats as stats
-import pandas as pd
 
 class Brownian:
-    def __init__(self, time_to_maturity, nb_step , nb_trajectoire, seed):
+    def __init__(self, time_to_maturity: float, nb_step: int, nb_trajectoire: int, seed: int) -> None:
         self.nb_step = nb_step
         self.nb_trajectoire = nb_trajectoire
         self.step = time_to_maturity / self.nb_step
         self.seed = seed
         # self._generator : np.random.Generator = np.random.default_rng(self.seed)
-        # print(self.seed)
         np.random.seed(self.seed)
 
-    def Scalaire(self):
-        # Mouvement Brownien
+    def Scalaire(self) -> np.ndarray:
+        # Mouvement Brownien scalaire
         W = np.zeros(self.nb_step+1) 
         for i in range(1,self.nb_step+1): 
             uniform_samples = np.random.uniform(0, 1)
             W[i] = W[i-1]+stats.norm.ppf(uniform_samples) * np.sqrt(self.step)
         return W
 
-    def Vecteur(self):
+    def Vecteur(self)-> np.ndarray:
         # Génération vectorielle des mouvements browniens
         uniform_samples = np.random.uniform(0, 1, (self.nb_trajectoire,self.nb_step)) 
         Z = stats.norm.ppf(uniform_samples)
         dW = np.sqrt(self.step) * Z
-        
-        # Construction du mouvement brownien
         W = np.zeros((self.nb_trajectoire, self.nb_step+1))
         W[:, 1:] = np.cumsum(dW, axis=1)
                 
